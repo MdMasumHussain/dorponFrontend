940e540c77e1a7f48b028e673794fbe6bf1e069c
Md Masum Hossain <97906106+MdMasumHussain@users.noreply.github.com>
2025-07-23 01:31:01 +0600

    Update carousel.js

--- src/app/lib/carousel.js
+++ src/app/lib/carousel.js
@@ -1,5 +1,5 @@
 export async function faceCarousel() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_USER_URL}/auth/carousel`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_USER_URL}/carousel`)
     const carousel = await res.json()
     return carousel;
 }
